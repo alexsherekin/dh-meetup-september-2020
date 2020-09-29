3a3fead733eebcf3eb918e26fc68e0406c93da81
Oleksandr Sherekin <oleksandr.sherekin@deliveryhero.com>
2020-09-29 14:59:54 +0200

    Small adjustment

--- strict-function-types/example.ts
+++ strict-function-types/example.ts
@@ -15,5 +15,6 @@ declare let f2: (x: Dog) => void;
 declare let f3: (x: Cat) => void;
 
 f1 = f2; // Strict mode only error. Type '(x: Dog) => void' is not assignable to type '(x: Animal) => void'
+         // f1(new Cat());
 f2 = f1; // Ok
 f2 = f3; // Type '(x: Cat) => void' is not assignable to type '(x: Dog) => void'
